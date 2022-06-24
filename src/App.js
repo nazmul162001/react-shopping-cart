014418a4e133ab2511aacc0bafd9f0fe0540ba70
nazmul162001 <nazmulprime162001@gmail.com>
2022-06-25 01:39:32 +0600

    handle add & remove cart & design cart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, {useState} from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Product from "./components/Product/Product";
+import React, { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Product from './components/Product/Product';
+import SingleCart from './components/SingleCart/SingleCart';
 
 function App() {
   const [show, setShow] = useState(true);
   const [cart, setCart] = useState([]);
+
   const handleAddToCart = (item) => {
-    setCart([...cart, item])
-  }  
-  
-  
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+
+  // useEffect(() => {
+  //   console.log("cart change");
+  // }, [cart]);
+
   return (
     <div className="">
-      <Navbar setShow={setShow}/>
-      {
-        show? <Product handleAddToCart={handleAddToCart} /> : "cart"
-      }
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Product handleAddToCart={handleAddToCart} />
+      ) : (
+        <SingleCart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Navbar.css';
 
-const Navbar = ({setShow}) => {
+const Navbar = ({setShow, size}) => {
   return (
     <nav className='w-full h-16 bg-sky-400'>
       <div className="nav_box w-4/5 m-auto flex items-center justify-between leading-[60px]">
         <span className="my_shop text-2xl font-bold text-white font-mono cursor-pointer" onClick={()=>setShow(true)}>My Shopping</span>
         <div onClick={()=>setShow(false)} className="cart cursor-pointer">
           <span className='text-4xl text-white'><i class="fas fa-cart-plus"></i></span>
-          <span className='px-1 text-white bg-red-600 font-bold rounded-lg relative top-[-15px] right-[5px]'>1</span>
+          <span className='px-1 text-white bg-red-600 font-bold rounded-lg relative top-[-15px] right-[5px]'>{size}</span>
         </div>
       </div>
     </nav>
